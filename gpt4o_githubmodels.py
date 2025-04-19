43ebd1f15cd5c9a4d229239843003b3d5fcd5f0a
jairo neder monassa <84083197+desenvolvedorjairomonassa@users.noreply.github.com>
2025-04-19 15:03:52 -0300

    Create gpt4o_githubmodels.py

---
+++ gpt4o_githubmodels.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+
+from azure.ai.inference import ChatCompletionsClient
+from azure.ai.inference.models import SystemMessage, UserMessage
+from azure.core.credentials import AzureKeyCredential
+load_dotenv()
+client = ChatCompletionsClient(
+    endpoint="https://models.inference.ai.azure.com",
+    credential=AzureKeyCredential(os.environ["GITHUB_TOKEN"]),
+)
+# Obtém o diretório do script atual
+diretorio_atual = os.path.dirname(os.path.abspath(__file__))
+caminho_arquivo = os.path.join(diretorio_atual, "mensagens.txt")
+# ler um arquivo de texto com mensagens
+with open(caminho_arquivo, "r", encoding="utf-8") as file:
+    mensagens = file.read()
+
+
+response = client.complete(
+    messages=[
+        SystemMessage(content="Você é um assistente que vai verificar a conversa entre adolescentes é sobre cometer suicídio. Classifique assim como alerta sucídio ou não alerta suicídio."),
+        UserMessage(content=mensagens),
+    ],
+    model=os.getenv("GITHUB_MODEL", "gpt-4o"),
+)
+print(response.choices[0].message.content)
